21636e86dd1bc0ec782e53c2675c0527999ea6fa
hojung7 <wjdghwjd99@naver.com>
2024-08-28 23:02:10 +0900

    20240828_User 관리프로그

--- jdbc2/src/edu/kh/jdbc/run/UserRun.java
+++ jdbc2/src/edu/kh/jdbc/run/UserRun.java
@@ -1,14 +1,13 @@
 package edu.kh.jdbc.run;
 
-import edu.kh.jdbc.common.JDBCTemplate;
 import edu.kh.jdbc.view.UserView;
 
 public class UserRun {
 	public static void main(String[] args) {
-		System.out.println(JDBCTemplate.getConnection());
 		
 		UserView view = new UserView();
-		view.test();
+//		view.test();
+		view.mainMenu();
 		
 	}
 }
